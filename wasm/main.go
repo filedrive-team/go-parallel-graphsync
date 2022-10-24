d46e4af1d32e782593cb4ece7523a2dd31dd1c48
wpg <deanqs@qq.com>
2022-10-24 16:02:50 +0800

    feat:add webassembly for generateSelectors

--- wasm/main.go
+++ wasm/main.go
@@ -6,11 +6,15 @@ package main
 import (
 	"fmt"
 	"github.com/filedrive-team/go-parallel-graphsync/util"
+	"github.com/filedrive-team/go-parallel-graphsync/util/parseselector"
+	"github.com/ipld/go-ipld-prime/codec/dagjson"
+	selectorparse "github.com/ipld/go-ipld-prime/traversal/selector/parse"
+	"strings"
 	"syscall/js"
 )
 
 func unionPathSelector() js.Func {
-	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	unionPathSelectorFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		var input []string
 		for _, a := range args {
 			input = append(input, a.String())
@@ -23,10 +27,43 @@ func unionPathSelector() js.Func {
 		}
 		return node
 	})
-	return jsonFunc
+	return unionPathSelectorFunc
 }
 
+func generateSelectors() js.Func {
+	parseSelectorFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+
+		fmt.Printf("input %s\n", args[0].String())
+		node, err := selectorparse.ParseJSONSelector(args[0].String())
+		edge, nedge, err := parseselector.GenerateSelectors(node)
+		if err != nil {
+			fmt.Printf("unable to generate selectors %s\n", err)
+			return err.Error()
+		}
+		var edges, nedges []string
+		for _, n := range edge {
+			var edgeStr strings.Builder
+			dagjson.Encode(n, &edgeStr)
+			edges = append(edges, edgeStr.String())
+		}
+		for _, n := range nedge {
+			var nedgeStr strings.Builder
+			dagjson.Encode(n, &nedgeStr)
+			nedges = append(nedges, nedgeStr.String())
+		}
+		selectors := struct {
+			edges  []string
+			nedges []string
+		}{
+			edges:  edges,
+			nedges: nedges,
+		}
+		return selectors
+	})
+	return parseSelectorFunc
+}
 func main() {
 	fmt.Println("Go Web Assembly")
 	js.Global().Set("unionPathSelector", unionPathSelector())
+	js.Global().Set("generateSelectors", generateSelectors())
 }
