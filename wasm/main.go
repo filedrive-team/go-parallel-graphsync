93bcd35888b5e68795730a603036ffbdf3f92f98
wpg <deanqs@qq.com>
2022-10-27 09:24:54 +0800

    feat:add wasm for parseComplexSelectors

--- wasm/main.go
+++ wasm/main.go
@@ -13,57 +13,55 @@ import (
 	"syscall/js"
 )
 
-func unionPathSelector() js.Func {
-	unionPathSelectorFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		var input []string
-		for _, a := range args {
-			input = append(input, a.String())
-		}
-		fmt.Printf("input %s\n", input)
-		node, err := util.UnionPathSelector(input)
-		if err != nil {
-			fmt.Printf("unable to union path to selector %s\n", err)
-			return err.Error()
-		}
-		return node
-	})
-	return unionPathSelectorFunc
+func unionPathSelector(this js.Value, args []js.Value) interface{} {
+	var input []string
+	for _, a := range args {
+		input = append(input, a.String())
+	}
+	fmt.Printf("input %s\n", input)
+	node, err := util.UnionPathSelector(input)
+	if err != nil {
+		fmt.Printf("unable to union path to selector %s\n", err)
+		return err.Error()
+	}
+	var s strings.Builder
+	dagjson.Encode(node, &s)
+	return js.ValueOf(s.String())
 }
 
-func generateSelectors() js.Func {
-	parseSelectorFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+func parseComplexSelectors(this js.Value, args []js.Value) interface{} {
 
-		fmt.Printf("input %s\n", args[0].String())
-		node, err := selectorparse.ParseJSONSelector(args[0].String())
-		edge, nedge, err := parseselector.GenerateSelectors(node)
-		if err != nil {
-			fmt.Printf("unable to generate selectors %s\n", err)
-			return err.Error()
-		}
-		var edges, nedges []string
-		for _, n := range edge {
-			var edgeStr strings.Builder
-			dagjson.Encode(n, &edgeStr)
-			edges = append(edges, edgeStr.String())
-		}
-		for _, n := range nedge {
-			var nedgeStr strings.Builder
-			dagjson.Encode(n, &nedgeStr)
-			nedges = append(nedges, nedgeStr.String())
-		}
-		selectors := struct {
-			edges  []string
-			nedges []string
-		}{
-			edges:  edges,
-			nedges: nedges,
-		}
-		return selectors
-	})
-	return parseSelectorFunc
+	fmt.Printf("input %s\n", args[0].String())
+	node, err := selectorparse.ParseJSONSelector(args[0].String())
+	edge, nedge, err := parseselector.GenerateSelectors(node)
+	if err != nil {
+		fmt.Printf("unable to generate selectors %s\n", err)
+		return err.Error()
+	}
+	var edges, nedges []string
+	for _, n := range edge {
+		var edgeStr strings.Builder
+		dagjson.Encode(n, &edgeStr)
+		edges = append(edges, edgeStr.String())
+	}
+	for _, n := range nedge {
+		var nedgeStr strings.Builder
+		dagjson.Encode(n, &nedgeStr)
+		nedges = append(nedges, nedgeStr.String())
+	}
+	selectors := struct {
+		edges  []string
+		nedges []string
+	}{
+		edges:  edges,
+		nedges: nedges,
+	}
+	return js.ValueOf(selectors)
+
 }
 func main() {
-	fmt.Println("Go Web Assembly")
-	js.Global().Set("unionPathSelector", unionPathSelector())
-	js.Global().Set("generateSelectors", generateSelectors())
+	done := make(chan int, 0)
+	js.Global().Set("unionPathSelector", js.FuncOf(unionPathSelector))
+	js.Global().Set("parseComplexSelectors", js.FuncOf(parseComplexSelectors))
+	<-done
 }
