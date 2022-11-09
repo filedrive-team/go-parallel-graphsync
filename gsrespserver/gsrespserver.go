8ac7695fdecc264e2e97417db0f76a63dfd8804e
wpg <deanqs@qq.com>
2022-11-09 18:18:25 +0800

    fix:lock when remove

--- gsrespserver/gsrespserver.go
+++ gsrespserver/gsrespserver.go
@@ -17,7 +17,10 @@ type ParallelGraphServerManger struct {
 	ParaGraphServers map[string]*ParallelGraphServer
 }
 type ParallelGraphServer struct {
-	lock           sync.Mutex
+	lock     sync.RWMutex
+	peerInfo PeerInfo
+}
+type PeerInfo struct {
 	dealCount      int64
 	timeDelay      int64
 	transformSpeed uint64
@@ -28,9 +31,10 @@ func NewParallelGraphServerManger(infos []peer.AddrInfo, host host.Host) *Parall
 	var parallelGraphServerManger ParallelGraphServerManger
 	parallelGraphServers := make(map[string]*ParallelGraphServer)
 	for _, info := range infos {
-		parallelGraphServers[info.ID.String()] = &ParallelGraphServer{
+		parallelGraphServers[info.ID.String()] = &ParallelGraphServer{peerInfo: PeerInfo{
 			dealCount: 0,
 			addrInfo:  info,
+		},
 		}
 	}
 	parallelGraphServerManger.ParaGraphServers = parallelGraphServers
@@ -39,27 +43,38 @@ func NewParallelGraphServerManger(infos []peer.AddrInfo, host host.Host) *Parall
 }
 func (pm *ParallelGraphServerManger) UpdateSpeed(ctx context.Context, peerId string, transformSpeed uint64) {
 	pm.ParaGraphServers[peerId].lock.Lock()
-	pm.ParaGraphServers[peerId].transformSpeed = transformSpeed
+	pm.ParaGraphServers[peerId].peerInfo.transformSpeed = transformSpeed
 	pm.ParaGraphServers[peerId].lock.Unlock()
 	//fmt.Printf("peerId %v,speed: %d\n", peerId, transformSpeed)
 }
 func (pm *ParallelGraphServerManger) UpdateDelay(ctx context.Context, peerId string, timeDelay int64) {
 	pm.ParaGraphServers[peerId].lock.Lock()
-	pm.ParaGraphServers[peerId].timeDelay = timeDelay
+	pm.ParaGraphServers[peerId].peerInfo.timeDelay = timeDelay
 	pm.ParaGraphServers[peerId].lock.Unlock()
 }
 func (pm *ParallelGraphServerManger) RemovePeer(ctx context.Context, peerId string) {
+	pm.lock.Lock()
 	delete(pm.ParaGraphServers, peerId)
+	pm.lock.Unlock()
 }
 func (pm *ParallelGraphServerManger) UpdateDealCount(ctx context.Context, peerId string, count int64) {
 	pm.ParaGraphServers[peerId].lock.Lock()
-	pm.ParaGraphServers[peerId].dealCount = count
+	pm.ParaGraphServers[peerId].peerInfo.dealCount = count
 	pm.ParaGraphServers[peerId].lock.Unlock()
+}
+func (pm *ParallelGraphServerManger) GetPeerInfo(ctx context.Context, peerId string) PeerInfo {
+	pm.ParaGraphServers[peerId].lock.RLock()
+	defer pm.ParaGraphServers[peerId].lock.RUnlock()
+	info, ok := pm.ParaGraphServers[peerId]
+	if ok {
+		return info.peerInfo
+	}
+	return PeerInfo{}
 }
 func (pm *ParallelGraphServerManger) FreeDealCount(ctx context.Context, params []pargraphsync.RequestParam) {
 	for _, param := range params {
 		pm.ParaGraphServers[param.PeerId.String()].lock.Lock()
-		pm.ParaGraphServers[param.PeerId.String()].dealCount--
+		pm.ParaGraphServers[param.PeerId.String()].peerInfo.dealCount--
 		pm.ParaGraphServers[param.PeerId.String()].lock.Unlock()
 	}
 }
@@ -69,13 +84,13 @@ func (pm *ParallelGraphServerManger) GetIdlePeer(ctx context.Context) peer.ID {
 	dealCount := int64(1_000_000)
 	var peerId peer.ID
 	for _, pgDServer := range pm.ParaGraphServers {
-		if pgDServer.dealCount < dealCount {
-			dealCount = pgDServer.dealCount
-			peerId = pgDServer.addrInfo.ID
+		if pgDServer.peerInfo.dealCount < dealCount {
+			dealCount = pgDServer.peerInfo.dealCount
+			peerId = pgDServer.peerInfo.addrInfo.ID
 		}
 	}
 	//fmt.Println("IdlePeer:", peerId.String())
-	pm.ParaGraphServers[peerId.String()].dealCount++
+	pm.ParaGraphServers[peerId.String()].peerInfo.dealCount++
 	return peerId
 }
 func (pm *ParallelGraphServerManger) GetPeerCount(ctx context.Context) int {
@@ -92,15 +107,15 @@ func (pm *ParallelGraphServerManger) RecordDelay(ctx context.Context, pingInterv
 		case <-timer.C:
 			for _, pgs := range pm.ParaGraphServers {
 				ps := ping.NewPingService(pm.host)
-				ts := ps.Ping(ctx, pgs.addrInfo.ID)
+				ts := ps.Ping(ctx, pgs.peerInfo.addrInfo.ID)
 				select {
 				case res := <-ts:
 					if res.Error != nil {
 						fmt.Println("err:", res.Error)
 						continue
 					}
-					fmt.Println("ping took: ", res.RTT.Nanoseconds())
-					pm.UpdateDelay(ctx, pgs.addrInfo.ID.String(), res.RTT.Nanoseconds())
+					fmt.Printf("ID:%v,ping took:%v\n", pgs.peerInfo.addrInfo.ID, res.RTT.Nanoseconds())
+					pm.UpdateDelay(ctx, pgs.peerInfo.addrInfo.ID.String(), res.RTT.Nanoseconds())
 
 				case <-time.After(time.Second * 2):
 					fmt.Println("failed to receive ping")
